7dd255eaf19bd8e4114f5e4b0e4a41c6ff9bbde6
Kamy <Kamy@4Ever.mynetworksettings.com>
2024-01-07 13:42:01 -0500

    Initial Commit

---
+++ src/f/DemoF.java
@@ -0,0 +1,16 @@
+package f;
+
+public class DemoF {
+
+	public static void main(String[] args) {
+
+		System.out.println("Yahya");
+		System.out.println("Kamy");
+
+	}
+
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
